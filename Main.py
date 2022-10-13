6dcf1077c3c5a38061df220b96c3d306eabb920d
Christian Glaeser <Christian.glaeser05@gmail.com>
2022-10-13 12:25:37 +0200

    Player

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
 class House(object):
     def __init__(self,list_player,card_deck):
 
+class Player(object):
+    def __init__(self, player_num, cards):
+        self.player_num = player_num
+        self.cards = cards
+    
+    def check_cards(self.cards):
+        print(self.cards)
+        
+    def stay():
+        
+    def card(self.cards):
+        
+    
