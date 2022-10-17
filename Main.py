c28702a93d83a41f03cd0786a252e9c724d9cce9
Philipp PLAPPART <phil.plap@lfw.lyceefrancais.at>
2022-10-17 13:51:25 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,25 @@
+cards = {"KoS":10,"QoS":10,"JoS":10,"10oS":10,
+        "9oS":9,"8oS":8,"7oS":7,"6oS":6,"5oS":5,"4oS":4,"3oS":3,"2oS":2,"AoS":1,
+        "KoH":10,"QoH":10,"JoH":10,"10oH":10,
+        "9oH":9,"8oH":8,"7oH":7,"6oH":6,"5oH":5,"4oH":4,"3oH":3,"2oH":2,"AoH":1,
+        "KoC":10,"QoC":10,"JoC":10,"10oC":10,
+        "9oC":9,"8oC":8,"7oC":7,"6oC":6,"5oC":5,"4oC":4,"3oC":3,"2oC":2,"AoC":1,
+        "KoD":10,"QoD":10,"JoD":10,"10oD":10,
+        "9oD":9,"8oD":8,"7oD":7,"6oD":6,"5oD":5,"4oD":4,"3oD":3,"2oD":2,"AoD":1}
+
 class House(object):
     def __init__(self,list_player,card_deck):
+        self.ingame = list_player
 
 class Player(object):
     def __init__(self, player_num, cards):
         self.player_num = player_num
         self.cards = cards
-    
+
     def check_cards(self.cards):
         print(self.cards)
-        
+
     def stay():
-        
+
     def card(self.cards):
-        
-    
+
