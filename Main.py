4f764da93141245858a94dc4112de137b92c7036
phil-plap <96175980+phil-plap@users.noreply.github.com>
2022-11-13 14:05:17 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #Imports
 from random import shuffle
 
-#Variables 
+#Variables
 game = True
 cards = {"KoS":10,"QoS":10,"JoS":10,"10oS":10,
         "9oS":9,"8oS":8,"7oS":7,"6oS":6,"5oS":5,"4oS":4,"3oS":3,"2oS":2,"AoS":1,
@@ -66,35 +66,169 @@ class House(object):
         else:
             print("le jeu est terminer")
             print("Le gagnant est:")
-            a = [players_stay[i][1] for i in range(len(players_stay)) if players_stay[i][0].eliminer == False]
-            p = max(a)
-            for i in range(len(players_stay)):
-                if players_stay[i][1] == p:
-                    b = players_stay[i][0]
-            print("joueur ", b.player_num, " avec ", p, " points")
+            if len(players_stay) != 1:
+                a = [players_stay[i][1] for i in range(len(players_stay)) if players_stay[i][0].eliminer == False]
+                p = max(a)
+                for i in range(len(players_stay)):
+                    if players_stay[i][1] == p:
+                        b = players_stay[i][0]
+                print("joueur ", b.player_num, " avec ", p, " points")
+            else:
+                b = players_stay[0][0]
+                print("joueur ",b.plyer_num," avec ", players_stay[0][1], " points")
 
 
 
 #Game
-'''
-Christian = Player(1)
-Philipp = Player(2)
-'''
-Banque = House()
+#Initialisation des joueurs
+nb_joueur = int(input("combien de joueur? Maximum 5 joueur "))
+if nb_joueur == 1:
+    j1 = Player(1,input("Quel est votre nom? "))
+    Banque = House([j1.name])
+
+if nb_joueur == 2:
+    j1 = Player(1,input("Quel est votre nom joueur 1? "))
+    j2 = Player(2,input("Quel est votre nom joueur 2? "))
+    Banque = House([j1.name,j2.name])
+
+if nb_joueur == 3:
+    j1 = Player(1,input("Quel est votre nom joueur 1? "))
+    j2 = Player(2,input("Quel est votre nom joueur 2? "))
+    j3 = Player(3,input("Quel est votre nom joueur 3? "))
+    Banque = House([j1.name,j2.name,j3.name])
+
+if nb_joueur == 4:
+    j1 = Player(1,input("Quel est votre nom joueur 1? "))
+    j2 = Player(2,input("Quel est votre nom joueur 2? "))
+    j3 = Player(3,input("Quel est votre nom joueur 3? "))
+    j4 = Player(4,input("Quel est votre nom joueur 4? "))
+    Banque = House([j1.name,j2.name,j3.name,j4.name])
+
+if nb_joueur == 5:
+    j1 = Player(1,input("Quel est votre nom joueur 1? "))
+    j2 = Player(2,input("Quel est votre nom joueur 2? "))
+    j3 = Player(3,input("Quel est votre nom joueur 3? "))
+    j4 = Player(4,input("Quel est votre nom joueur 4? "))
+    j5 = Player(5,input("Quel est votre nom joueur 5? "))
+    Banque = House([j1.name,j2.name,j3.name,j4.name,j5.name])
+
 Banque.Start_game()
+while len(Banque.ingame) != len(players_stay):
+    print("0 - Prendre une carte?")
+    print("1 - Rester?")
 
+    if len(Banque.ingame) == 1:
+        for i in range(len(Banque.ingame) +1):
+            if i == j1.player_num:
+                choice = int(input("Que voulez vous faire ?"+  str(j1.name)))
+                if choice == 0:
+                    j1.card()
+                else:
+                    j1.stay()
 
-while game == True:
-    
-    num_play = int(input("Combien de joueurs? "))
-    for i in range(num_play):
-        nom = input("Quelle est votre nom? ")
-        nom = Player(i + 1, nom)
-    
-    for i in range(num_play):
-        print("0 - Prendre une carte?")
-        print("1 - Rester?")
-        choice = int(input("Que voulez vous faire? "))
-        if choice == 0:
-            nom.card()
-            print(nom.score, nom.hand, Player(1))
+    if len(Banque.ingame) == 2:
+        for i in range(len(Banque.ingame) +1):
+            if i == j1.player_num:
+                choice = int(input("Que voulez vous faire ?"+  str(j1.name)))
+                if choice == 0:
+                    j1.card()
+                else:
+                    j1.stay()
+
+            if i == j2.player_num:
+                choice = int(input("Que voulez vous faire " +  str( j2.name)))
+                if choice == 0:
+                    j2.card()
+                else:
+                    j2.stay()
+
+    if len(Banque.ingame) == 3:
+        for i in range(len(Banque.ingame) +1):
+            if i == j1.player_num:
+                choice = int(input("Que voulez vous faire ?"+  str(j1.name)))
+                if choice == 0:
+                    j1.card()
+                else:
+                    j1.stay()
+
+            if i == j2.player_num:
+                choice = int(input("Que voulez vous faire " +  str( j2.name)))
+                if choice == 0:
+                    j2.card()
+                else:
+                    j2.stay()
+
+            if i == j3.player_num:
+                choice = int(input("Que voulez vous faire ?" + str( j3.name)))
+                if choice == 0:
+                    j3.card()
+                else:
+                    j3.stay()
+
+    if len(Banque.ingame) == 4:
+        for i in range(len(Banque.ingame) +1):
+            if i == j1.player_num:
+                choice = int(input("Que voulez vous faire ?"+  str(j1.name)))
+                if choice == 0:
+                    j1.card()
+                else:
+                    j1.stay()
+
+            if i == j2.player_num:
+                choice = int(input("Que voulez vous faire " +  str( j2.name)))
+                if choice == 0:
+                    j2.card()
+                else:
+                    j2.stay()
+
+            if i == j3.player_num:
+                choice = int(input("Que voulez vous faire ?" + str( j3.name)))
+                if choice == 0:
+                    j3.card()
+                else:
+                    j3.stay()
+
+            if i == j4.player_num:
+                choice = int(input("Que voulez vous faire ?"+ str( j4.name)))
+                if choice == 0:
+                    j4.card()
+                else:
+                    j4.stay()
+
+    if len(Banque.ingame) == 5:
+        for i in range(len(Banque.ingame) +1):
+            if i == j1.player_num:
+                choice = int(input("Que voulez vous faire ?"+  str(j1.name)))
+                if choice == 0:
+                    j1.card()
+                else:
+                    j1.stay()
+
+            if i == j2.player_num:
+                choice = int(input("Que voulez vous faire " +  str( j2.name)))
+                if choice == 0:
+                    j2.card()
+                else:
+                    j2.stay()
+
+            if i == j3.player_num:
+                choice = int(input("Que voulez vous faire ?" + str( j3.name)))
+                if choice == 0:
+                    j3.card()
+                else:
+                    j3.stay()
+
+            if i == j4.player_num:
+                choice = int(input("Que voulez vous faire ?"+ str( j4.name)))
+                if choice == 0:
+                    j4.card()
+                else:
+                    j4.stay()
+
+            if i == j5.player_num:
+                choice = int(input("Que voulez vous faire ?"+ str(j5.name)))
+                if choice == 0:
+                    j5.card()
+                else:
+                    j5.stay()
+Banque.Check()
